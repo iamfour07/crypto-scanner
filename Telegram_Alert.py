370c26be4ca066943dc6634cde8653e79d3072ee
iamfour07 <64779453+iamfour07@users.noreply.github.com>
2025-09-08 12:43:13 +0530

    Update Telegram_Alert.py

--- Telegram_Alert.py
+++ Telegram_Alert.py
@@ -1,5 +1,4 @@
 import os
-import html
 
 import requests
 bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
@@ -9,7 +8,7 @@ def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {
         "chat_id": chat_id,
-        "text": html.escape(message),  # Escape special characters like < > &
+        "text": message,  # Escape special characters like < > &
     }
     try:
         response = requests.post(url, data=payload)
