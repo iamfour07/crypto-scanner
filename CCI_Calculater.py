6dcde17e519e8b09abdc8dcae93e38bf48225c9e
iamfour07 <64779453+iamfour07@users.noreply.github.com>
2025-09-30 18:28:02 +0530

    update branch

--- CCI_Calculater.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def calculate_cci(high, low, close, period=20):
-    tp = (high + low + close) / 3
-    sma = tp.rolling(period).mean()
-    mean_dev = tp.rolling(period).apply(lambda x: (x - x.mean()).abs().mean(), raw=False)  # raw=False!
-    cci = (tp - sma) / (0.015 * mean_dev)
-    return cci.round(2)
---
+++ Telegram_Alert_Swing.py
@@ -0,0 +1,24 @@
+
+import os
+import requests
+bot_token = os.environ.get("TELEGRAM_BOT_TOKEN_Swing")
+chat_id = os.environ.get("TELEGRAM_CHAT_ID_Swing") #
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": message,  # Escape special characters like < > &
+    }
+    try:
+        response = requests.post(url, data=payload)
+        if response.status_code == 200:
+            print("✅ Message sent successfully")
+        else:
+            print("⚠️ Failed to send message:", response.text)
+    except Exception as e:
+        print("⚠️ Error:", e)
+
+
+
+
