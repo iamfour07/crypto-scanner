1cf8d44ea7c87771a69d4c932aa744da077493b7
iamfour07 <64779453+iamfour07@users.noreply.github.com>
2025-10-13 23:10:54 +0530

    Update ReversalBuySell.py

--- ReversalBuySell.py
+++ ReversalBuySell.py
@@ -7,7 +7,7 @@ import requests
 import pandas as pd
 from datetime import datetime, timezone, timedelta
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from Telegram_Alert import send_telegram_message
+from Telegram_Alert_Swing import send_telegram_message
 
 # =========================
 # CONFIGURATION
