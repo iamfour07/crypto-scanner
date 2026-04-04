18eb73efd09427dd0662cc12de4886b022675505
ravi.sharma <ravi.sharma@sarvika.com>
2026-04-04 19:46:09 +0530

    updated coin length

--- NewMomentum.py
+++ NewMomentum.py
@@ -77,11 +77,11 @@ def main():
     if net_diff >= 33:
         mode = "BUY"
         # SKIP TOP 3 (iloc[3:18]), SCAN NEXT 15
-        scan_list = df_all.sort_values("change", ascending=False).iloc[3:15]
+        scan_list = df_all.sort_values("change", ascending=False).iloc[3:20]
     elif net_diff <= -33:
         mode = "SELL"
         # SKIP TOP 3 (iloc[3:18]), SCAN NEXT 15
-        scan_list = df_all.sort_values("change", ascending=True).iloc[3:15]
+        scan_list = df_all.sort_values("change", ascending=True).iloc[3:20]
     else:
         print(f"Neutral Market ({net_diff:.1f}%). Waiting for clear 33% trend.")
         return
